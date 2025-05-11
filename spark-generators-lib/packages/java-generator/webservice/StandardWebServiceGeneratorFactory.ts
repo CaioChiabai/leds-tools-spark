4e784cfcba807fca11ab88355b18b80655c974ac
Caio Chiabai <caiochiabaioliveira@gmail.com>
2025-05-11 11:26:06 -0300

    Modificado nome de WebServiceGeneratorFactory para AbstractWebServiceGeneratorFactory

--- spark-generators-lib/packages/java-generator/webservice/StandardWebServiceGeneratorFactory.ts
+++ spark-generators-lib/packages/java-generator/webservice/StandardWebServiceGeneratorFactory.ts
@@ -2,7 +2,7 @@ import { IJavaGenerator } from "../interfaces/IJavaGenerator.js";
 import { ConfigGenerator } from "./ConfigGenerator.js";
 import { ModuleGenerator } from "./ModuleGenerator.js";
 import { GraphQLGenerator } from "./GraphQLGenerator.js";
-import { WebServiceGeneratorFactory } from "./WebServiceGeneratorFactory.js";
+import { WebServiceGeneratorFactory } from "./AbstractWebServiceGeneratorFactory.js";
 
 export class StandardWebServiceGeneratorFactory extends WebServiceGeneratorFactory {
     protected createGenerators(): IJavaGenerator[] {
